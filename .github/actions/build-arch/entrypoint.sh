5a4ec30a2e017edc84ab778c3541a5aef0e20a01
Daniel <dhaavi@users.noreply.github.com>
2021-03-08 14:51:58 +0100

    Set $PKGDEST to a writable location

--- .github/actions/build-arch/entrypoint.sh
+++ .github/actions/build-arch/entrypoint.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
 set -e
-
 cd linux
+
+# Create user "docker" because makepkg refuses to run as root.
 id docker || (echo creating user docker; useradd docker)
 chown -R docker:docker .
 
-# Reset PKGEXT to it's default (it's different in the build-container)
-PKGEXT=".pkg.tar.xz" su docker -c makepkg
+# Set PKGDEST to a location where user "docker" can write.
+export PKGDEST="$(pwd)/pkg"
+mkdir -p $PKGDEST
+# Reset PKGEXT to it's default (it's different in the build-container).
+export PKGEXT=".pkg.tar.xz"
 
+# Build package.
+su docker -c makepkg
+
+# Check result and prepare for uploading.
 ls -lah
-
 pkgname=$(ls *.pkg.tar*)
 mv $pkgname ../$pkgname
 echo ::set-output name=filename::$pkgname
