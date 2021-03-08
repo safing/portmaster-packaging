af67f640595b75f8339d82d439f62c30f02fe8d4
Daniel <dhaavi@users.noreply.github.com>
2021-03-08 13:06:08 +0100

    Update to portmaster-start v0.5.1

--- linux/build.sh
+++ linux/build.sh
@@ -3,9 +3,11 @@
 # Check for prerequisites.
 if [[ $(which convert) == "" ]]; then
   echo "cannot find command convert, please install imagemagick"
+  exit 1
 fi
 if [[ $(which dpkg-buildpackage) == "" ]]; then
   echo "cannot find command dpkg-buildpackage, please install debhelper"
+  exit 1
 fi
 
 dpkg-buildpackage --no-sign
