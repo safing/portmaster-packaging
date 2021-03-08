aab822f0026e6a76a1fdcf16a72762c00b93ce5d
Daniel <dhaavi@users.noreply.github.com>
2021-03-08 15:04:58 +0100

    Try another $PKGDEST dir

--- .github/actions/build-arch/entrypoint.sh
+++ .github/actions/build-arch/entrypoint.sh
@@ -5,15 +5,13 @@ cd linux
 
 # Create user "docker" because makepkg refuses to run as root.
 id docker || (echo creating user docker; useradd docker)
+chown -R docker:docker .
 
 # Set PKGDEST to a location where user "docker" can write.
-export PKGDEST="$(pwd)/pkg"
+export PKGDEST="/tmp/pkg"
 mkdir -p $PKGDEST
 # Reset PKGEXT to it's default (it's different in the build-container).
 export PKGEXT=".pkg.tar.xz"
-
-# Transfer dir ownership to user "docker" for building.
-chown -R docker:docker .
 
 # Build package.
 su docker -c makepkg
