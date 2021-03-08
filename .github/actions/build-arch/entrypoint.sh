3ceb180c598c58275b30a05391c94043ff23e872
Daniel <dhaavi@users.noreply.github.com>
2021-03-08 14:03:39 +0100

    Create user docker if not exists

--- .github/actions/build-arch/entrypoint.sh
+++ .github/actions/build-arch/entrypoint.sh
@@ -3,9 +3,11 @@
 set -e
 
 cd linux
+id docker || (echo creating user docker; useradd docker)
+chown -R docker:docker .
 
 # Reset PKGEXT to it's default (it's different in the build-container)
-PKGEXT=".pkg.tar.xz" makepkg
+PKGEXT=".pkg.tar.xz" su docker -c makepkg
 
 ls -lah
 
