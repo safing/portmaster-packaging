0d50e54be90be96186010c08c3ddfacc72569ec3
Safing <noc@safing.io>
2023-03-01 11:10:46 +0100

    Fix file permissions

--- linux/templates/create-archive.sh
+++ linux/templates/create-archive.sh
@@ -30,6 +30,14 @@ done
 # Copy the arch.install file into our assets
 cp archive.install "${pkgdir}/.INSTALL.sh"
 
+# Fix file permissions for archive.
+(
+    cd ${pkgdir} &&
+    find . -type d -exec chmod 0755 {} \; &&
+    find . -type f -exec chmod 0644 {} \; &&
+    find . -type f -name "*.sh" -exec chmod 0755 {} \;
+)
+
 # Create filelist to support easy uninstallation
 # like doing a `cat /opt/safing/portmaster/.installed-files | sudo xargs rm`
 ( cd ${pkgdir} && find . -type f ) >> ${pkgdir}/.installed-files
